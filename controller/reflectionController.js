39a30c7f91cff58a33fda42aa064b14863af0a59
CPuPush <fpakpahan44@gmail.com>
2022-10-18 17:54:43 +0700

    FINAL

--- controller/reflectionController.js
+++ controller/reflectionController.js
@@ -1,18 +1,18 @@
+const { json } = require('express');
 const db = require('../config/db');
 
 class reflectionController{
   // 
   static async getAllReflection(req, res){
     try {
-      const idUser = res.dataUser.id;
-      console.log(idUser);
+      const owner_id = res.dataUser.id;
       let result = await db.query(
-        `select * from reflections where owner_id = ($1);`, [idUser]
+        `select * from reflections where owner_id = ($1);`, [owner_id]
       );
       let results = result.rows;
-      res.status(200).json(results);
+      return res.status(200).json(results);
     } catch (error) {
-      res.status(500).json(error);
+      return res.status(500).json(error);
     }
   }
 
@@ -22,7 +22,7 @@ class reflectionController{
       const dataReflectionId = await db.query(
         `SELECT * FROM reflections where id=($1);`, [id]
       );
-      res.status(200).json(dataReflectionId.rows[0]);
+      return res.status(200).json(dataReflectionId.rows[0]);
     }catch(error){
       res.status(500).json(error);
     } 
@@ -30,9 +30,52 @@ class reflectionController{
 
   static async createReflection(req, res){
     try {
+      const {success, low_point, take_away, created_date, modified_date} = req.body;
+      const owner_id = res.dataUser.id;
+
+      if(!success || !low_point || !take_away || !created_date || !modified_date){
+        return res.status(400).json("Bad Request");
+      }
+
+      await db.query(
+        `INSERT INTO reflections (success, low_point, take_away, owner_id, created_date, modified_date) VALUES ($1, $2, $3, $4, $5, $6);`,
+        [success, low_point, take_away, owner_id, created_date, modified_date]
+      );
+      return res.status(201).json({message: "Reflection has been created"})
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  }
+
+  static async editReflectionById(req, res){
+    try {
+      const {success, low_point, take_away, created_date, modified_date} = req.body;
+      const id = req.params.id;
+      
+      if(!success || !low_point || !take_away || !created_date || !modified_date){
+        return res.status(400).json("Bad Request");
+      }
+
+      await db.query(
+        `UPDATE reflections SET success=$1,low_point=$2, take_away=$3,created_date=$4, modified_date=$5 WHERE id=$6;`,
+        [success, low_point, take_away, created_date, modified_date,id]
+      );
+      return res.status(200).json({message: "Update Reflection Successfully"});
       
     } catch (error) {
-      res.status(500).json(error);
+      return res.status(500).json(error);
+    }
+  }
+
+  static async deleteReflectionById(req, res){
+    try {
+      const {id} = req.params;
+      await db.query(
+        `DELETE FROM reflections WHERE id=$1;`,[id]
+      );
+      return res.status(200).json({message: "Delete Successfully"});
+    } catch (error) {
+      return res.status(500).json(error);
     }
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -11,14 +11,23 @@ router.post("/users/login", userController.login);
 router.get("/reflections", authentication, reflectionController.getAllReflection);
 router.get("/reflections/:id", authentication, authorization, reflectionController.getReflectionById);
 router.post("/reflections", authentication, reflectionController.createReflection);
-
+router.put("/reflections/:id", authentication, authorization, reflectionController.editReflectionById);
+router.delete("/reflections/:id", authentication, authorization, reflectionController.deleteReflectionById);
 
 
 module.exports = router;
 
 
 
-
+// {
+//   "id": 1,
+//   "success": "test",
+//   "low_point": "test",
+//   "take_away": "test",
+//   "owner_id": 9,
+//   "created_date": "2010-10-09T17:00:00.000Z",
+//   "modified_date": "2010-10-09T17:00:00.000Z"
+// }
 
 
 

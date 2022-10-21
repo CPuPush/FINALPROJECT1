3b2db0f9eb858acef1e150c9b01ad223e4adbb52
CPuPush <fpakpahan44@gmail.com>
2022-10-21 10:57:50 +0700

    env

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const routes = require("./routes");
+require('dotenv').config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
